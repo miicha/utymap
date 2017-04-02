5b004dc496b98edff2c23a00abfdc067dfa611fe
Ilya Builuk <ilya.builuk@gmail.com>
2017-04-02 21:48:35 +0200

    demo: configure gestures speed in surface scene

--- unity/demo/Assets/Scripts/Scene/Gestures/SurfaceGestureStrategy.cs
+++ unity/demo/Assets/Scripts/Scene/Gestures/SurfaceGestureStrategy.cs
@@ -6,8 +6,13 @@ namespace Assets.Scripts.Scene.Gestures
 {
     internal class SurfaceGestureStrategy : GestureStrategy
     {
-        private float _panSpeed = 1f;
-        private float _zoomSpeed = 100f;
+        private const float PanMaxSpeed = 1f;
+        private const float PanMinSpeed = 0.005f;
+        private const float PanFactor = 0.05f;
+
+        private const float ZoomMaxSpeed = 100f;
+        private const float ZoomMinSpeed = 1f;
+        private const float ZoomFactor = 0.2f;
 
         public SurfaceGestureStrategy(TileController tileController, 
                                       ScreenTransformGesture twoFingerMoveGesture,
@@ -18,12 +23,14 @@ namespace Assets.Scripts.Scene.Gestures
 
         public override void OnManipulationTransform(Transform pivot, Transform camera)
         {
-            pivot.localPosition += new Vector3(ManipulationGesture.DeltaPosition.x, 0, ManipulationGesture.DeltaPosition.y) * -_panSpeed;
+            var speed = Mathf.Max(PanMaxSpeed * InterpolateByZoom(PanFactor), PanMinSpeed);
+            pivot.localPosition += new Vector3(ManipulationGesture.DeltaPosition.x, 0, ManipulationGesture.DeltaPosition.y) * -speed;
         }
 
         public override void OnTwoFingerTransform(Transform pivot, Transform camera)
         {
-            pivot.localPosition += Vector3.up * (1 -TwoFingerMoveGesture.DeltaScale) * _zoomSpeed;
+            var speed = Mathf.Max(ZoomMaxSpeed * InterpolateByZoom(ZoomFactor), ZoomMinSpeed);
+            pivot.localPosition += Vector3.up * (1 - TwoFingerMoveGesture.DeltaScale) * speed;
         }
     }
 }
