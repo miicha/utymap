4861519460bb42243208de9d8cdf987a26b2a11e
Ilya Builuk <ilya.builuk@gmail.com>
2017-04-09 14:29:03 +0200

    core: add unit test for parsing way using osm xml parser

--- core/test/formats/osm/xml/OsmXmlParserTest.cpp
+++ core/test/formats/osm/xml/OsmXmlParserTest.cpp
@@ -1,5 +1,6 @@
 #include "BoundingBox.hpp"
 #include "entities/Node.hpp"
+#include "entities/Way.hpp"
 #include "formats/osm/xml/OsmXmlParser.hpp"
 
 #include "config.hpp"
@@ -32,8 +33,8 @@ namespace {
         });
     }
 
-    void assertTags(const std::vector<utymap::entities::Tag> expectedTags,
-        const std::vector<utymap::entities::Tag> actualTags)
+    void assertTags(const std::vector<utymap::entities::Tag>& expectedTags,
+                    const std::vector<utymap::entities::Tag>& actualTags)
     {
         BOOST_CHECK_EQUAL(expectedTags.size(), actualTags.size());
         for (int i = 0; i < actualTags.size(); ++i)
@@ -43,13 +44,30 @@ namespace {
         }
     }
 
-    void assertNode(const Node& node,
-        const utymap::GeoCoordinate coordinate,
-        const std::vector<utymap::entities::Tag> tags)
+    void assertGeometry(const std::vector<utymap::GeoCoordinate>& expectedGeometry,
+                        const std::vector<utymap::GeoCoordinate>& actualGeometry)
     {
-        BOOST_CHECK_EQUAL(coordinate.latitude, node.coordinate.latitude);
-        BOOST_CHECK_EQUAL(coordinate.longitude, node.coordinate.longitude);
+        BOOST_CHECK_EQUAL(expectedGeometry.size(), actualGeometry.size());
+        for (int i = 0; i < actualGeometry.size(); ++i)
+        {
+            BOOST_CHECK_EQUAL(expectedGeometry.at(i).latitude, actualGeometry.at(i).latitude);
+            BOOST_CHECK_EQUAL(expectedGeometry.at(i).longitude, actualGeometry.at(i).longitude);
+        }
+    }
+
+    void assertNode(const Node& node,
+                    const utymap::GeoCoordinate& coordinate,
+                    const std::vector<utymap::entities::Tag>& tags)
+    {
+        assertGeometry({ coordinate }, { node.coordinate });
         assertTags(tags, node.tags);
+    }
+
+    void assertWay(const Way& way,
+                   const std::vector<utymap::GeoCoordinate>& geometry,
+                   const std::vector<utymap::entities::Tag>& tags) {
+        assertGeometry(geometry, way.coordinates);
+        assertTags(tags, way.tags);
     }
 }
 
@@ -77,12 +95,45 @@ BOOST_AUTO_TEST_CASE(GivenTestDummyXml_WhenParserParse_ThenHasProperNodes)
     OsmDataVisitor visitor(*dependencyProvider.getStringTable(), [&](Element& element) {
         if (Node* node = dynamic_cast<Node*>(&element)) {
             if (node->id == 42421951) {
-                assertNode(*node, utymap::GeoCoordinate(40.7033242, -74.0077787), { createTag("highway","traffic_signals") });
+                assertNode(*node,
+                    utymap::GeoCoordinate(40.7033242, -74.0077787),
+                    { createTag("highway","traffic_signals") });
                 checkList[0] = true;
             }
             else if (node->id == 42424605) {
                 assertNode(*node, utymap::GeoCoordinate(40.8142100, -73.9341897), {});
                 checkList[1] = true;
+            }
+        }
+        return true;
+    });
+
+    parser.parse(istream, visitor);
+    visitor.complete();
+
+    BOOST_CHECK(reduce(checkList.begin(), checkList.end()));
+}
+
+BOOST_AUTO_TEST_CASE(GivenTestDummyXml_WhenParserParse_ThenHasProperWay)
+{
+    std::vector<bool> checkList { false };
+    std::ifstream istream(TEST_OSM_DUMMY_FILE, std::ios::in);
+    OsmXmlParser<OsmDataVisitor> parser;
+    OsmDataVisitor visitor(*dependencyProvider.getStringTable(), [&](Element& element) {
+        if (Way* way = dynamic_cast<Way*>(&element)) {
+            if (way->id == 32934353) {
+                assertWay(*way,
+                    { 
+                        utymap::GeoCoordinate(40.7033242, -74.0077787),
+                        utymap::GeoCoordinate(40.8142100, -73.9341897)
+                    },
+                    { 
+                        // NOTE eating spaces is known issue
+                        createTag("alt_name", "FranklinDelanoRooseveltDrive"),
+                        createTag("lanes", "3"),
+                        createTag("tiger:reviewed", "no")
+                    });
+                checkList[0] = true;
             }
         }
         return true;
