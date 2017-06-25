9a1ace71bcfe454f04ddef57ebdfa149edd8a2b5
Ilya Builuk <ilya.builuk@gmail.com>
2017-06-25 20:13:22 +0200

    mapcss: use original tiles and render all roads

--- unity/library/UtyMap.Unity/Data/Providers/Geo/MapzenMapDataProvider.cs
+++ unity/library/UtyMap.Unity/Data/Providers/Geo/MapzenMapDataProvider.cs
@@ -14,8 +14,7 @@ namespace UtyMap.Unity.Data.Providers.Geo
         /// <summary> Provides the way to control what to request from server. </summary>
         private static readonly Dictionary<int, int> LodMapping = new Dictionary<int, int>()
         {
-            {2, 1}, {3, 3}, {4, 3}, {5, 4}, {6, 5}, {7, 6}, {8, 7},
-            {9, 8}, {10, 9}, {11, 10}, {12, 11}
+            {2, 1}, {3, 3}, {4, 3}, {5, 4}, {6, 5}, {7, 6}, {8, 7}
         };
 
         private string _importPath;
