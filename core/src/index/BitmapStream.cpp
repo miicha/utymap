622197c99369d6cffe273b30b989b86a0b2e8827
Ilya Builuk <ilya.builuk@gmail.com>
2017-09-30 20:18:18 +0200

    core: add bitmap index

---
+++ core/src/index/BitmapStream.cpp
@@ -0,0 +1,11 @@
+#include "index/BitmapStream.hpp"
+
+using namespace utymap::index;
+
+void BitmapStream::read(std::istream &in, StringIndex::Bitmap &bitmap) {
+
+}
+
+void BitmapStream::write(std::ostream &out, const StringIndex::Bitmap &bitmap) {
+
+}
---
+++ core/src/index/BitmapStream.hpp
@@ -0,0 +1,24 @@
+#ifndef INDEX_BITMAPSTREAM_HPP_DEFINED
+#define INDEX_BITMAPSTREAM_HPP_DEFINED
+
+#include <iostream>
+#include "StringIndex.hpp"
+
+namespace utymap {
+namespace index {
+
+class BitmapStream final {
+ public:
+  /// Reads bitmap from stream.
+  static void read(std::istream &in,
+                   StringIndex::Bitmap &bitmap);
+
+  /// Writes bitmap to stream.
+  static void write(std::ostream &out,
+                    const StringIndex::Bitmap &bitmap);
+};
+
+}
+}
+
+#endif // INDEX_BITMAPSTREAM_HPP_DEFINED
